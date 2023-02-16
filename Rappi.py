2b5300bfdeda59070ea38d6f59ccfacd7f88d3a6
Iram Mendoza <123773810+IramAnt@users.noreply.github.com>
2023-02-16 01:05:45 -0600

    Veamos si funciona

--- Rappi.py
+++ Rappi.py
@@ -1,4 +1,3 @@
-import Asistente_Rappi as Asistente
 #LISTAS Y VARIABLES A UTILIZAR
 # [Nombre, Antiguedad, Clave de Departamento]
 empleados = {
@@ -13,9 +12,75 @@ info_privada_empleados = {
     920: [7000, 5631341770, "Richard o Robin #2"]
 }
 #______________________
+#______________________ MOSTRAR EMPLEADO
+def mostrar_empleado ():
+    if id_empleado in empleados:
+        nombre, antiguedad, clave = empleados[id_empleado]
+        print(f"Nombre del empleado: {nombre}")
+        print(f"Antiguedad: {antiguedad} años")
+        print(f"Clave de Departamento : {clave}")
+        empleado = [nombre, antiguedad, clave]
+        return empleado
+    else:
+        print(f"El ID de empleado {id_empleado} no se encuentra en la lista.")
+        pass
+#_______________________
+#_______________________ MOSTRAR VACACIONES
+def mostrar_vacaciones (x):
+    print(f"El empleado : {empleado[0]} tendra este 2023",x,"dias de vacaciones")
 
+def clave_empleado1 (empleado):
+    if empleado[1] == 1:
+        return 6
+    elif empleado[1] >= 2 and empleado [1] <= 6:
+        return 14
+    elif empleado[1] == 0:
+        return 0
+    else:
+        return 20
+
+def clave_empleado2 (empleado):
+    if empleado[1] == 0:
+        return 0
+    elif empleado[1] == 1:
+        return 7
+    elif empleado[1] >= 2 and empleado [1] <= 6:
+        return 15
+    else:
+        return 22
+
+def clave_empleado3 (empleado):
+    if empleado [1] == 0:
+        return 0
+    elif empleado [1] == 1:
+        return 10
+    elif empleado [1] >= 2 and empleado <= 6:
+        return 20
+    else:
+        return 30
+
+def calculador_vacacional (empleado):
+    if empleado[1] == 1 and empleado [2] == 1:
+        print(f"El empleado : {empleado[0]} debe tener este 2023 : 6 dias de vacaciones")
+    if empleado[1] == 2 and empleado [2] == 1:
+        print(f"El empleado : {empleado[0]} debe tener este 2023 : 14 dias de vacaciones") 
+#_______________________
+#_______________________ MOSTRAR INFORMACION PERSONAL
+def mostrar_info_personal ():
+    contraseña = "1825"
+    print("Introduzca su contraseña : ")
+    contraseña_usuario = str(input())
+    if contraseña_usuario not in contraseña: 
+        print("Contraseña incorrecta")
+    if contraseña_usuario == contraseña:
+        if id_empleado in info_privada_empleados:
+            sueldo, celular, direccion = info_privada_empleados [id_empleado]
+            print(f"Sueldo : {sueldo}")
+            print(f"No. Telefonico : {celular}")
+            print(f"Direccion : {direccion}")
+            return empleado
 #----------------------- AMBIENTE GLOBAL AMBIENTE GLOBAL AMBIENTE GLOBAL ----------------
-Asistente = Asistente
+
 #______________________
 
 #QUE EMPLEADO SE CONSULTARA --- GLOBAL
@@ -25,7 +90,7 @@ empleado = mostrar_empleado()
 #___________________________
 
 #QUE DESEA CONSULTAR SOBRE EL EMPLEADO --- GLOBAL
-print ("Que deseas saber o hacer acerca de la informacion del empleado?")
+print ("¿Que deseas saber o hacer acerca de la informacion del empleado?")
 funcion_a_realizar = str(input())
 funcion = funcion_a_realizar.lower()
 #___________________________
@@ -44,7 +109,5 @@ if "vacaciones" in funcion.split():
 #____________________________
 
 #MOSTRAR INFOMACION PERSONAL --- GLOBAL
-if "info personal" or "informacion personal" or "personal" in funcion:
+if "info personal" or "informacion personal" or "personal" in funcion.split():
     mostrar_info_personal()
-# Esto muestra que mis ideas sobre un commit funciona!
-#VEAMOS
